11ae8e49ca027afcadebf466a2fc3fb3ca770e21
always-Txz <3555096051@qq.com>
2025-10-09 17:01:09 +0800

    Site updated: 2025-10-09 17:01:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759999598825";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760000455556";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
